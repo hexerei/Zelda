c731ec3b9d849dc0cfbead531184b54ab5756c14
Daniel Vorhauer <daniel.vorhauer@dxc.com>
2022-08-18 14:25:15 +0200

    weapon creates damage on enemy

--- src/enemy.py
+++ src/enemy.py
@@ -38,6 +38,11 @@ class Enemy(Entity):
         self.attack_time = None
         self.attack_cooldown = 400
 
+        # invincibiilty timer
+        self.vulnerable = True
+        self.hit_time = None
+        self.invincibility_duration = 300
+
     def import_graphics(self, name):
         self.animations = {'idle': [], 'move': [], 'attack': []}
         main_path = f'../gfx/monsters/{name}/'
@@ -59,10 +64,13 @@ class Enemy(Entity):
         self.rect = self.image.get_rect(center = self.hitbox.center)
 
     def cooldown(self):
+        current_time = pygame.time.get_ticks()
         if not self.can_attack:
-            current_time = pygame.time.get_ticks()
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.can_attack = True
+        if not self.vulnerable:
+            if current_time - self.hit_time >= self.invincibility_duration:
+                self.vulnerable = True
 
     def get_player_distance_direction(self, player):
         dist_vec = pygame.math.Vector2(player.rect.center) - pygame.math.Vector2(self.rect.center)
@@ -94,10 +102,30 @@ class Enemy(Entity):
         else:
             self.direction = pygame.math.Vector2()
 
+    def get_damage(self, player, attack_type):
+        if self.vulnerable:
+            self.direction = self.get_player_distance_direction(player)[1]
+            if attack_type == 'weapon':
+                self.health -= player.get_full_weapon_damage()
+            else:
+                pass
+            self.hit_time = pygame.time.get_ticks()
+            self.vulnerable = False
+
+    def hit_reaction(self):
+        if not self.vulnerable:
+            self.direction *= -self.resistance
+
+    def check_death(self):
+        if self.health <= 0:
+            self.kill()
+
     def update(self):
+        self.hit_reaction()
         self.move(self.speed)
         self.animate()
         self.cooldown()
+        self.check_death()
 
     def enemy_update(self, player):
         self.get_status(player)
--- src/level.py
+++ src/level.py
@@ -15,7 +15,11 @@ class Level:
 		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
+		
 		self.current_attack = None
+		self.attack_sprites = pygame.sprite.Group()
+		self.attackable_sprites = pygame.sprite.Group()
+
 
 		# sprite setup
 		self.create_map()
@@ -50,7 +54,9 @@ class Level:
 
 						if style == 'grass':
 							random_grass_image = choice(graphics['grass'])
-							Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+							Tile((x,y), 
+								[self.visible_sprites, self.obstacle_sprites, self.attackable_sprites], 
+								'grass', random_grass_image)
 
 						if style == 'object':
 							Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', graphics['object'][int(col)])
@@ -70,12 +76,12 @@ class Level:
 								elif col == '391': monster_name = 'spirit'
 								elif col == '392': monster_name = 'raccoon'
 								else: monster_name = 'squid'
-								Enemy(monster_name, (x,y), [self.visible_sprites], self.obstacle_sprites)
+								Enemy(monster_name, (x,y), [self.visible_sprites, self.attackable_sprites], self.obstacle_sprites)
 
 
 
 	def create_attack(self):
-		self.current_attack = Weapon(self.player,[self.visible_sprites])
+		self.current_attack = Weapon(self.player,[self.visible_sprites, self.attack_sprites])
 
 	def create_magic(self, style, strength, cost):
 		print(style, strength, cost)
@@ -85,11 +91,25 @@ class Level:
 			self.current_attack.kill()
 		self.current_attack = None
 
+	def player_attack_logic(self):
+		if self.attack_sprites:
+			for attack_sprite in self.attack_sprites:
+				collision_sprites = pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, False)
+				if collision_sprites:
+					for target_sprite in collision_sprites:
+						if target_sprite.sprite_type == 'grass':
+							target_sprite.kill()
+						elif target_sprite.sprite_type == 'enemy':
+							target_sprite.get_damage(self.player, attack_sprite.sprite_type)
+						else:
+							pass
+
 	def run(self):
 		# update and draw the game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
 		self.visible_sprites.enemy_update(self.player)
+		self.player_attack_logic()
 		self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
